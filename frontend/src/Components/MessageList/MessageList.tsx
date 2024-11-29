e55838a2ee418d6a727076a63ee4a1e289be0dfa
Kashubyak Mykola <kashubyak514@gmail.com>
2024-11-29 13:59:09 +0200

    Removed unnecessary icons

--- frontend/src/Components/MessageList/MessageList.tsx
+++ frontend/src/Components/MessageList/MessageList.tsx
@@ -151,23 +151,12 @@ const MessageList = () => {
 				leftItems={[]}
 				rightItems={[
 					<ToolbarButton key='info' icon='ion-ios-information-circle-outline' />,
-					<ToolbarButton key='video' icon='ion-ios-videocam' />,
-					<ToolbarButton key='phone' icon='ion-ios-call' />,
 				]}
 			/>
 
 			<div className='messageListContainer'>{renderMessages()}</div>
 
-			<Compose
-				rightItems={[
-					<ToolbarButton key='photo' icon='ion-ios-camera' />,
-					<ToolbarButton key='image' icon='ion-ios-image' />,
-					<ToolbarButton key='audio' icon='ion-ios-mic' />,
-					<ToolbarButton key='money' icon='ion-ios-card' />,
-					<ToolbarButton key='games' icon='ion-logo-game-controller-b' />,
-					<ToolbarButton key='emoji' icon='ion-ios-happy' />,
-				]}
-			/>
+			<Compose rightItems={[<ToolbarButton key='send' icon='ion-ios-send' />]} />
 		</div>
 	)
 }
