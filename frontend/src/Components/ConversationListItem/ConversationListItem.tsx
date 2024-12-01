01b511800679af17dfa37a9d8c73042e13e654e0
Kashubyak Mykola <kashubyak514@gmail.com>
2024-12-01 23:51:50 +0200

    Set a backup user photo in case of an incorrect URL

--- frontend/src/Components/ConversationListItem/ConversationListItem.tsx
+++ frontend/src/Components/ConversationListItem/ConversationListItem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import shave from 'shave'
 import { IConversationItem } from '../../Types/Components.interface'
 import './ConversationListItem.css'
@@ -7,11 +7,21 @@ const ConversationListItem: React.FC<IConversationItem> = ({ data }) => {
 	useEffect(() => {
 		shave('.conversationSnippet', 20)
 	}, [])
-
 	const { username, avatar_url } = data
+	const fallbackAvatar =
+		'https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png'
+	const [imageSrc, setImageSrc] = useState(avatar_url)
+	const handleImageError = () => {
+		setImageSrc(fallbackAvatar)
+	}
 	return (
 		<div className='conversationListItem'>
-			<img className='conversationPhoto' src={avatar_url} alt='conversation' />
+			<img
+				className='conversationPhoto'
+				src={imageSrc}
+				alt='conversation'
+				onError={handleImageError}
+			/>
 			<div className='conversationInfo'>
 				<h1 className='conversationTitle'>{username}</h1>
 				{/* <p className='conversationSnippet'>{text}</p> */}
