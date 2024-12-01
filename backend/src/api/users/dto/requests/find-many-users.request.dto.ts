0bc1878672073aac1842765b8848ab812603b57d
Victor Artus <vartus2@gmail.com>
2024-12-01 12:59:09 +0200

    corrected find many users to make username filter optional

--- backend/src/api/users/dto/requests/find-many-users.request.dto.ts
+++ backend/src/api/users/dto/requests/find-many-users.request.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export default class FindManyUsersRequestDto {
   @ApiProperty({
@@ -8,5 +8,6 @@ export default class FindManyUsersRequestDto {
     required: true,
   })
   @IsString()
-  username: string;
+  @IsOptional()
+  username?: string;
 }
