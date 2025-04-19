4efb67485ed55f2b3b7146340b929abe65a407e9
kashubyak Mykola <kashubyak514@gmail.com>
2025-04-19 17:42:29 +0300

    Added adaptive (horizontal scroll) for the theme switcher.

--- frontend/src/Components/Settings/Themes/Themes.tsx
+++ frontend/src/Components/Settings/Themes/Themes.tsx
@@ -5,22 +5,20 @@ const Themes = () => {
 	const { setActiveTheme } = useThemeStore()
 
 	return (
-		<>
-			<div className='themeSwitcher'>
-				<div className='themeItem' onClick={() => setActiveTheme('lightTheme')}>
-					<img src='/lightTheme.png' alt='Day Mode' />
-					<p className='nameMode'>Day</p>
-				</div>
-				<div className='themeItem' onClick={() => setActiveTheme('darkTheme')}>
-					<img src='/darkTheme.png' alt='Night Mode' />
-					<p className='nameMode'>Night</p>
-				</div>
-				<div className='themeItem' onClick={() => setActiveTheme('systemTheme')}>
-					<img src='/systemTheme.png' alt='System Mode' />
-					<p className='nameMode'>System</p>
-				</div>
+		<div className='themeSwitcher'>
+			<div className='themeItem' onClick={() => setActiveTheme('lightTheme')}>
+				<img src='/lightTheme.png' alt='Day Mode' />
+				<p className='nameMode'>Day</p>
 			</div>
-		</>
+			<div className='themeItem' onClick={() => setActiveTheme('darkTheme')}>
+				<img src='/darkTheme.png' alt='Night Mode' />
+				<p className='nameMode'>Night</p>
+			</div>
+			<div className='themeItem' onClick={() => setActiveTheme('systemTheme')}>
+				<img src='/systemTheme.png' alt='System Mode' />
+				<p className='nameMode'>System</p>
+			</div>
+		</div>
 	)
 }
 
