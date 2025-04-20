ef47f5b211d881d24501fade56dd3149869d7502
kashubyak Mykola <kashubyak514@gmail.com>
2025-04-20 23:02:59 +0300

    Added the ability to write a message text in the chat without focusing on the input field (user convenience)

--- frontend/src/Components/Chats/Compose/Compose.tsx
+++ frontend/src/Components/Chats/Compose/Compose.tsx
@@ -1,23 +1,33 @@
 import { ICompose } from '@/Types/Components.interface'
 import { ToolbarButton } from '@/UI/ToolbarButton/ToolbarButton'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './Compose.css'
 
 const Compose: React.FC<ICompose & { onSend: (message: string) => void }> = ({
 	onSend,
 }) => {
 	const [input, setInput] = useState('')
-
+	const inputRef = useRef<HTMLInputElement>(null)
 	const handleSend = () => {
 		if (input.trim()) {
 			onSend(input)
 			setInput('')
 		}
 	}
+	useEffect(() => {
+		const handleGlobalKeyDown = (e: KeyboardEvent) => {
+			const isInputFocused = document.activeElement === inputRef.current
+			if (!isInputFocused && e.key.length === 1 && !e.ctrlKey && !e.metaKey && !e.altKey)
+				inputRef.current?.focus()
+		}
+		window.addEventListener('keydown', handleGlobalKeyDown)
+		return () => window.removeEventListener('keydown', handleGlobalKeyDown)
+	}, [])
 
 	return (
 		<div className='compose'>
 			<input
+				ref={inputRef}
 				type='text'
 				className='composeInput'
 				placeholder='Write a message...'
